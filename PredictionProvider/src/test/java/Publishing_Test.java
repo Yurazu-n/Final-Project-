d18641b83ca5bda99891dec0c1cdf637114f4bae
Owen <yurazunew@gmail.com>
2024-01-09 16:45:42 +0000

    Publishing_Test class added to PredictionProvider to test functionality

---
+++ PredictionProvider/src/test/java/Publishing_Test.java
@@ -0,0 +1,21 @@
+public class Publishing_Test {
+    public static void main(String[] args) {
+        /*
+        Runnable updateTask = () -> {
+            for (Location location : getLocations()) {
+                try {
+                    List<Weather> weathers = getWeatherProvider().getWeather(location);
+                    for (Weather weather : weathers) {
+                        getEventPublisher().publishWeather(weather);
+                    }
+                } catch (MyExecutionException e) {
+                    System.out.println("Execution Error");
+                }
+            }
+        };
+
+        scheduler.scheduleAtFixedRate(updateTask, 0, 3, TimeUnit.HOURS);
+
+ */
+    }
+}
