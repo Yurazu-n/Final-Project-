87dc1439c1b70e3d7f3ae2343b92fe9e9c4387b3
Owen <yurazunew@gmail.com>
2024-01-09 16:18:24 +0000

    Main classes of DatalakeBuilder & BusinessUnit stablished

--- BusinessUnit/src/main/java/org/owen/Main.java
+++ BusinessUnit/src/main/java/org/owen/Main.java
@@ -1,4 +1,13 @@
 package org.owen;
 
+import org.owen.control.BusinessUnitControl;
+import org.owen.control.BusinessUnitExecutionException;
+
 public class Main {
+    public static void main(String[] args) throws BusinessUnitExecutionException {
+        if (!args[0].isEmpty()) {
+            BusinessUnitControl businessUnitControl = new BusinessUnitControl(args[0]);
+            businessUnitControl.execute();
+        }
+    }
 }
--- DatalakeBuilder/src/main/java/org/owen/Main.java
+++ DatalakeBuilder/src/main/java/org/owen/Main.java
@@ -1,10 +1,12 @@
 package org.owen;
 
 import org.owen.control.DatalakeControl;
+import org.owen.control.MyEventException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyEventException {
         DatalakeControl datalakeControl = new DatalakeControl(args[0]);
+        datalakeControl.createDataLake();
     }
 }
